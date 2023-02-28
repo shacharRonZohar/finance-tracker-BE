39e7088a25e212e8eadcc88a9cbd414d7daadebd
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-02-28 23:02:47 +0200

    fixed env maybe

---
+++ dist/src/public/assets/HomePage-5d4eee47.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-c48bb216.js";const _=e({__name:"HomePage",setup(o){return(t,n)=>"home"}});export{_ as default};
--- dist/src/public/assets/HomePage-c207c031.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index-b80ea899.js";const _=e({__name:"HomePage",setup(o){return(t,n)=>"home"}});export{_ as default};
