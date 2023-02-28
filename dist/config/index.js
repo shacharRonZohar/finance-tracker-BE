bbb6ba0e136067a26a55450d4fb677fe468eb4d4
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-02-28 22:23:09 +0200

    frontend build

--- dist/config/index.js
+++
@@ -1,4 +0,0 @@
-export const config = {
-    dbURL: process.env.DB_URL || 'mongodb://localhost:27017',
-    dbName: 'financial_db',
-};
--- dist/src/api/user/user.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { requireAuth } from '../../middlewares/requireAuth.middleware.js';
-import { getUser, getUsers, updateUser } from './user.controller.js';
-export const userRouter = Router();
-// middleware that is specific to this router
-// router.use(requireAuth)
-userRouter.get('/', getUsers);
-userRouter.get('/:id', getUser);
-userRouter.put('/:id', requireAuth, updateUser);
