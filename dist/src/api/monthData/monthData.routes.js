1ae5a4e46965a5b48678b2e00644a79a05aa9944
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-02-28 22:38:35 +0200

    removed dist

--- dist/src/api/monthData/monthData.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { requireAuth } from '../../middlewares/requireAuth.middleware.js';
-import { getMonthData } from './monthData.controller.js';
-export const monthDataRouter = Router();
-// middleware that is specific to this router
-monthDataRouter.use(requireAuth);
-monthDataRouter.get('/:year', getMonthData);
-// monthlyDataRoutes.get('/:id', getUser)
-// monthlyDataRoutes.put('/:id',  updateUser)
