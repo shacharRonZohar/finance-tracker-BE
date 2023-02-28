bc4d966386ff026f8f79da3653dd0b2a4bba3626
Shachar Ron Zohar <88171079+shacharRonZohar@users.noreply.github.com>
2023-02-28 22:50:50 +0200

    new build

--- dist/src/public/assets/HomePage-0f53066b.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index-58c89ad4.js";const _=e({__name:"HomePage",setup(o){return(t,n)=>"home"}});export{_ as default};
---
+++ dist/src/public/assets/HomePage-c7877fe3.js
@@ -0,0 +1,1 @@
+import{d as e}from"./index-7357e0ca.js";const _=e({__name:"HomePage",setup(o){return(t,n)=>"home"}});export{_ as default};
